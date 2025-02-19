60e56eabb26bef13c8d9498bdf13d61a2e87a3a0
Oein <oein0219@gmail.com>
2025-02-19 09:15:47 +0900

    remove source

--- wasm/watch.sh
+++ wasm/watch.sh
@@ -1,2 +1,1 @@
-source ../
 nodemon -w src/* --exec "em++ src/main.cpp -o hello.html -O1 --shell-file html_template/shell_minimal.html -sEXPORTED_RUNTIME_METHODS=\"['cwrap','allocateUTF8','UTF8ToString']\" -sASSERTIONS"
